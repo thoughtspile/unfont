c883fbe6ad576e6e629d1cb280284b5d26d205b5
v.klepov <v.klepov@corp.vk.com>
2021-05-23 12:46:49 +0300

    add font to benchmarks

--- benchmark/webpack.config.js
+++ benchmark/webpack.config.js
@@ -1,14 +1,43 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  entry: {
+    'svg-js': path.join(__dirname, 'src', 'svg-js.js'),
+    'font': path.join(__dirname, 'src', 'font.css'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        type: "asset",
+      },
+    ],
+  },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: 'src/index.html' }),
-    new HtmlWebpackPlugin({ filename: 'inline.html', template: 'src/inline.html' }),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'svg-js.html',
+      chunks: ['svg-js'],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'font.html',
+      chunks: ['font'],
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'inline.html',
+      template: 'src/inline.html',
+      chunks: [],
+    }),
   ],
 }
